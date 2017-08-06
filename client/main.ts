d6f48defb63971c8e28edb08c8f9497ed41995a0
Florian Mettetal <florianmettetal.developer@gmail.com>
2017-08-06 10:49:32 -0700

    Meteor 1.5 TypeScript Local Module

--- client/main.ts
+++ client/main.ts
@@ -1,15 +1,11 @@
-import { Meteor } from 'meteor/meteor';
-
-import testJS from '0test-js'
+var testJS = require('0test-js');
 const testJSname = 'JS IMPORT TEST'
+
 const helloWorldTest = (module, name)=> console.log(module.hello('world') === 'hello world'? `${name} PASSED` : `${name} FAILED`)
+helloWorldTest(testJS, testJSname)
 
 
-import testTS from '0test-ts'
+var testTS = require('0test-ts')
 const testTSname = 'TS IMPORT TEST'
 const fooBarTest = (module, name) => console.log(module.foo+parseInt(module.bas) === (123+123)? `${name} PASSED` : `${name} FAILED`)
-
-Meteor.startup(() => {
-  helloWorldTest(testJS, testJSname)
-  fooBarTest(testTS, testTSname)
-});
+fooBarTest(testTS, testTSname)
