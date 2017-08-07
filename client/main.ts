8c8898d507d1739cff0d8e41e335d894bb689805
Florian Mettetal <florian.mettetal@flex.com>
2017-08-07 11:26:29 -0700

    Another way to import TS module, with gulp to build TS

--- client/main.ts
+++ client/main.ts
@@ -6,6 +6,12 @@ helloWorldTest(testJS, testJSname)
 
 
 var testTS = require('0test-ts')
-const testTSname = 'TS IMPORT TEST'
+const testTSname = 'TS0  IMPORT TEST'
 const fooBarTest = (module, name) => console.log(module.foo+parseInt(module.bas) === (123+123)? `${name} PASSED` : `${name} FAILED`)
 fooBarTest(testTS, testTSname)
+
+
+import * as testTS1 from '1test-ts'
+const testTS1name = 'TS1 IMPORT TEST'
+const addTest = (module, name) => console.log(module.add(2,3) === 5? `${name} PASSED` : `${name} FAILED`)
+addTest(testTS1, testTS1name)
